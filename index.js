4abef5d914c0e162d343135c8c2c50c7e9cd89eb
Martin Siesse <msiesse@student.42.fr>
2020-11-20 15:41:55 +0100

    Autre requête pour avoir accès à des produits, un peu plus de 130 produits, mais ce n'est pas la liste complète

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fs = require('fs');
 
 async function main() {
     const login = await (axios.post('https://ios-api-gateway.frichti.co/auth/token', {
@@ -15,13 +16,13 @@ async function main() {
        }
    }).catch(function(error) { console.log(error.response) });
 
-   const items = await axios.get('https://ios-api-gateway.frichti.co/v6/menu/hubs/4/slugs/plats-cuisines-group?rootslug=live', {
+   const items = await axios.get('https://ios-api-gateway.frichti.co/v6/menu?rootslug=live', {
     headers: {
         'Authorization': `Bearer ${Buffer.from(login.data.token).toString('base64')}`
        }
    }).catch(function(error) { console.log(error.response) });
 
-   console.log(items.data.collections.plats.items);
+   console.log(items.data.rulesItems.length);
 }
 
 main();
