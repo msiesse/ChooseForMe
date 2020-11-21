f5b4e5a8e9c44e750349719a54975c0dac4a5b47
Martin Siesse <msiesse@student.42.fr>
2020-11-21 19:34:47 +0100

    Premier test de commande random, j'affiche juste les produits, pas de lancement de commande

--- index.js
+++ index.js
@@ -22,28 +22,51 @@ async function main() {
     const address = await address_search.findAddress(content[2], content[3]);
     client.setAddress(address);
 
-    console.log(client.address);
-
     // Store tests
-  /*  const store = new Store(client);
+    const store = new Store(client);
     
-    products = await store.getProducts('live', 'plats-cuisines-group');*/
+    products = await store.getProducts('live', 'plats-cuisines-group');
+
+    const id = [];
+    products.forEach((product, index, array) => {
+        if (!id.includes(product.productId)) {
+            id.push(product.productId);
+        } else {
+            array.splice(index, 1);
+        }
+    });
+
+    let price = 0;
+    let category;
+    let randomProduct;
+    while (category !== 'PLAT') {
+        randomProduct = products[Math.floor(Math.random() * products.length)];
+        price = randomProduct.price;
+        category = randomProduct.category;
+    }
+
+    console.log(randomProduct);
+
+    price -= 800;
+    if (price < 0) {
+        let priceSide = 0;
+        while (priceSide >= (-price + 200) || priceSide < -price || category === 'PLAT') {
+            randomProduct = products[Math.floor(Math.random() * products.length)];
+            priceSide = randomProduct.price;
+            category = randomProduct.category;
+        }
+        console.log(randomProduct);
+    }
 
     // Order tests
-    const order = new Order(client);
+    /*const order = new Order(client);
 
     await order.getCart();
 
     const slot = await order.getFirstSlot();
 
     const ticket = await order.orderProduct('1000918');
-    const state = await order.checkout();
-
-    /*const id = [];
-    products.forEach(product => {
-        if (!id.includes(product.productId))
-            id.push(product.productId);
-    })*/
+    const state = await order.checkout();*/
 
 
 
